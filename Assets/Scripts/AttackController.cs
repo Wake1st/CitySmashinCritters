84132535a18a42bb095c2f3614518e79e7b43fcd
Joel Wakefield <joeltwakefield@gmail.com>
2023-01-02 16:43:45 -0700

    resolve #23 - updated attack system

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -2,9 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class AttackType {
+    public char keybinding;
+    public int damage;
+    public float cooldown;
+
+    public AttackType(char key, int dmg, float cool = 0) {
+        keybinding = key;
+        damage = dmg;
+
+        if (cool > 0) {
+            cooldown = cool;
+        } else {
+            cooldown = (float)dmg/10;
+        }
+    }
+}
+
+public class AttackProfile {
+    public AttackType lightPunch;
+    public AttackType lightKick;
+    public AttackType lightSpecial;
+    public AttackType heavyPunch;
+    public AttackType heavyKick;
+    public AttackType heavySpecial;
+
+    public AttackProfile() {
+        lightPunch = new AttackType('l', 6);
+        lightKick = new AttackType('k', 8);
+        lightSpecial = new AttackType('j', 12);
+        heavyPunch = new AttackType('o', 10);
+        heavyKick = new AttackType('i', 14);
+        heavySpecial = new AttackType('u', 20);
+    }
+}
+
 public class AttackController : MonoBehaviour
 {
-    public int damage = 10;
+    private AttackProfile attackProfile;
     public Animator animator;
 
     private float nextFire = 0.5F;
@@ -17,6 +52,10 @@ public class AttackController : MonoBehaviour
 
     private bool isPlaying;
 
+    private void Awake() {
+        attackProfile = new AttackProfile();
+    }
+
     private void Start() {
         LevelController.UpdateIsPlaying += SetIsPlaying;
     }
@@ -24,12 +63,22 @@ public class AttackController : MonoBehaviour
     void Update()
     {
         if (isPlaying) {
-            myTime = myTime + Time.deltaTime;
+            //  Check for blocking
+            if (Input.GetButton("Block")) {
+                print("blocking");
+                return;
+            }
 
+            //  Check for attack
+            myTime = myTime + Time.deltaTime;
             if (myTime > nextFire) {
-                if (Input.GetButton("Fire1")) {
-                    Attack();
+                AttackType attack = CheckAttack();
+
+                if (attack != null) {
+                    Attack(attack);
                     myTime = 0.0F;
+                    nextFire = attack.cooldown;
+                    print(nextFire);
                 } else {
                     animator.SetBool("Attacking", false);
                 }
@@ -51,12 +100,46 @@ public class AttackController : MonoBehaviour
         enemy = null;
     }
 
-    private void Attack() {
+    private AttackType CheckAttack() {
+        if (Input.GetButtonDown("LightPunch")) {
+            print("lightPunch");
+            return attackProfile.lightPunch;
+        }
+
+        if (Input.GetButtonDown("LightKick")) {
+            print("lightKick");
+            return attackProfile.lightKick;
+        }
+
+        if (Input.GetButtonDown("LightSpecial")) {
+            print("lightSpecial");
+            return attackProfile.lightSpecial;
+        }
+
+        if (Input.GetButtonDown("HeavyPunch")) {
+            print("heavyPunch");
+            return attackProfile.heavyPunch;
+        }
+
+        if (Input.GetButtonDown("HeavyKick")) {
+            print("heavyKick");
+            return attackProfile.heavyKick;
+        }
+
+        if (Input.GetButtonDown("HeavySpecial")) {
+            print("heavySpecial");
+            return attackProfile.heavySpecial;
+        }
+
+        return null;
+    }
+
+    private void Attack(AttackType attack) {
         if (enemy != null) {
             GameObject
                 .Find(enemy.name + "/Health")
                 .GetComponent<DestructableHealthController>()
-                .TakeDamage(damage);
+                .TakeDamage(attack.damage);
         }
 
         animator.SetBool("Attacking", true);
